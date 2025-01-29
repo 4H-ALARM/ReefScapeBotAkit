77df7783183c8c20e844398a75d3626cdef42d6a
Aditya Naravane <adityanaravane2008@gmail.com>
2025-01-29 17:08:01 -0500

    staring on sim

--- src/main/java/frc/robot/subsystems/climber/ClimberIOSim.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIOSim.java
@@ -4,13 +4,23 @@
 
 package frc.robot.subsystems.climber;
 
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import frc.robot.RobotConstants;
+
 /** Add your docs here. */
 public class ClimberIOSim {
-  // private final DCMotorSim motor;
+  private final DCMotorSim motor;
   // private final LoggedMechanism2d climberMechanism;
+  private final DCMotor gearbox = DCMotor.getKrakenX60Foc(1);
 
   public ClimberIOSim() {
-    // motor = new DCMotorSim(LinearSystemId.createDCMotorSystem());
-
+    motor =
+        new DCMotorSim(
+            LinearSystemId.createDCMotorSystem(
+                gearbox, 1, RobotConstants.ClimberConstants.sensorToMechanismRatio),
+            gearbox,
+            null);
   }
 }
